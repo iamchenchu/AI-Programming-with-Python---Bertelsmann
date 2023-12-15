141d2c7863a593aada7ed9ab5e461163253c6eb7
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-12-14 23:47:33 -0600

    Adde code for strings

---
+++ 01.OperatorsAndDataTypes/08strings.py
@@ -0,0 +1,6 @@
+coconut_count = "34"
+mango_count = "15"
+tropical_fruit_count = coconut_count + mango_count
+print(tropical_fruit_count)
+print(type(tropical_fruit_count))
+
---
+++ 01.OperatorsAndDataTypes/09type_coversion.py
@@ -0,0 +1,23 @@
+print(type(4))
+print(type(3.7))
+print(type('this'))
+print(type(True))
+print("0" + "5")
+print(0 + 5)
+
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+#TODO: assign the total sales to a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the assigning statement.
+result_string = f"This week's total sales: {int(mon_sales)+int(tues_sales)+int(wed_sales)+int(thurs_sales)+int(fri_sales)}"
+print(result_string)
+
+weekly_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+weekly_sales1 = str(weekly_sales)  #convert the type back!!
+result_string1 = "This week's total sales: " + weekly_sales1
+print(result_string1)
+
+
