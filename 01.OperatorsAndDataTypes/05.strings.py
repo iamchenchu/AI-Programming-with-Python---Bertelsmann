519d7e59812635411b9ca11b334956a6cd4d5a19
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-12-14 22:35:51 -0600

    Uploaded the code for strings

---
+++ 01.OperatorsAndDataTypes/05.strings.py
@@ -0,0 +1,20 @@
+# Data type for immutable ordered sequence of characters (Eg : Letters, numbers, spaces, and symbols)
+print("hello")
+name = "chenchu"
+print(name)
+question = 'all call him "chenchu", but what is his real name ?'
+print(question)
+
+#concatenation 
+first_name = "chenchu"
+last_name = " reddy"
+print(first_name+last_name)
+
+#String multiplication
+print(3*name)
+
+#Length of string
+print(len(name))
+
+this_string = 'Simon\'s skateboard is in the garage.'
+print(this_string)
---
+++ 01.OperatorsAndDataTypes/06.Strings.py
@@ -0,0 +1,11 @@
+first_word = 'Hello'
+second_word = 'There'
+print(first_word + second_word)
+print(first_word + ' ' + second_word)
+print(first_word * 5)
+print(len(first_word))
+print(first_word[0])
+print(first_word[1])
+print(len("ababa") / len("ab"))
+
+
