a783fb5d1a80a4efbe4ede4b01399b36f2d9b11b
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2023-12-15 23:03:40 -0600

    Updated the code for Strings

---
+++ 01.OperatorsAndDataTypes/10string_methods.py
@@ -0,0 +1,5 @@
+my_string = "sebastian tharun"
+print(my_string.title()) # makes the first letter to capital in each word
+print(my_string.islower()) #Checks all the string letters are lower case or not 
+print(my_string.count('s')) #counts the letter or word in the string
+print(my_string.find('s')) # returns the first time index of the position of the string
