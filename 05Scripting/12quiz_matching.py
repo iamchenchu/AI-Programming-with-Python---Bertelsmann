6fc4bb0bffca4e07481f6c4875760c27c9c53fc2
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-01-05 17:40:09 -0600

    Added the code

---
+++ 05Scripting/12quiz_matching.py
@@ -0,0 +1,13 @@
+"""NameError: name 'abc_dict' is not defined : 
+Identifier (name or variable) is not found in the local or global namespace. Make sure the reference to the identifier is correctly added 
+to the code.
+
+UnboundLocalError : 
+Local variable is accessed before it is defined. Make sure local scope of variable in function is defined or value assigned to it.
+
+ValueError: too many values to unpack (expected 2) :
+Assignment error. Check how many values are being unpacked and how many variables the values should be assigned to.
+
+TypeError: unsupported operand type(s) for +: 'int' and 'str' :
+An operation or function is applied to an object of inappropriate type - e.g., trying to concatenate a string and integer. 
+Change the datatype for one of the values (e.g., change int to str)"""
