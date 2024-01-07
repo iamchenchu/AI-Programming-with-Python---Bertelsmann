5b92c26b2182b3b79333c854bb5e64361e2f3700
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-01-07 10:36:27 -0600

    Added the code about classes in python

---
+++ 05Scripting/19package.py
@@ -0,0 +1,24 @@
+"""In order to manage the code better, modules in the Python Standard Library are split down into sub-modules that are contained within 
+a package. A package is simply a module that contains sub-modules. A sub-module is specified with the usual dot notation.
+
+Modules that are submodules are specified by the package name and then the submodule name separated by a dot. 
+You can import the submodule like this."""
+
+#    import package_name.submodule_name
+
+"""Importing and accessing from modules
+In this quiz, you'll be using different methods to import and use the random.randint() function from the random module.
+Your task is to match the import statement with the way you would then call the function itself."""
+
+"""Match the import statement with the way that random.randint() is called."""
+
+"""import random  -> random.randint(0,10)
+
+from random import randint -> randint(0,10)
+
+import random as rd -> rd.randint(0,10)
+
+from random import randint as rint -> rint(0,10)
+
+from random import *  -> Don't use this import statement!"""
+
---
+++ 05Scripting/20third_party_bibraries.py
@@ -0,0 +1,29 @@
+"""We can use this command to install 3rd party libraries : pip install package_name"""
+
+"""Usually the developers keep the names of all the third party libraries they used in requirements.txt, 
+You can install all the libraries at a time using the below command"""
+
+# pip install -r requirements.txt
+
+
+# Useful Third-Party Packages
+
+"""IPython - A better interactive Python interpreter
+requests - Provides easy to use methods to make web requests. Useful for accessing web APIs.
+Flask - a lightweight framework for making web applications and APIs.
+Django - A more featureful framework for making web applications. Django is particularly good for designing complex, 
+content heavy, web applications.
+Beautiful Soup - Used to parse HTML and extract information from it. Great for web scraping.
+pytest - extends Python's builtin assertions and unittest module.
+PyYAML - For reading and writing YAML files.
+NumPy - The fundamental package for scientific computing with Python. It contains among other things a powerful N-dimensional 
+array object and useful linear algebra capabilities.
+pandas - A library containing high-performance, data structures and data analysis tools. In particular, pandas provides dataframes!
+Matplotlib - a 2D plotting library which produces publication quality figures in a variety of hardcopy formats and interactive 
+environments.
+ggplot - Another 2D plotting library, based on R's ggplot2 library.
+Pillow - The Python Imaging Library adds image processing capabilities to your Python interpreter.
+pyglet - A cross-platform application framework intended for game development.
+Pygame - A set of Python modules designed for writing games.
+pytz - World Timezone Definitions for Python"""
+
