fa7aa664157f3cc0a6cf22b15c6239f4dd8e172a
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-01-29 16:58:44 -0600

    Added the code

--- 01OperatorsAndDataTypes/04integers_and_floats.py
+++ 01OperatorsAndDataTypes/04integers_and_floats.py
@@ -2,4 +2,9 @@ x = int(4.7)   # x is now an integer 4
 y = float(4)   # y is now a float of 4.0
 print(type(x))
 print(type(y))
+print(.1 + .1 + .1)
 print(.1 + .1 + .1 == .3) # False
+
+import math
+
+print(math.isclose(0.1 + 0.1 + 0.1, 0.3))
