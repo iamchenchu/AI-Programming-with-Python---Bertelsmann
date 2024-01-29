522c18f654a4eef519bfa632c121102e5fba18ef
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-01-29 16:51:26 -0600

    Added the code and made some modifications

--- 01OperatorsAndDataTypes/03boolean_check.py
+++ 01OperatorsAndDataTypes/03boolean_check.py
@@ -6,3 +6,4 @@ rio_de_janeiro_pop_density = rio_population/rio_area
 
 # Write code that gives a boolean value True if San Francisco is denser than Rio, and False otherwise
 comparision_result = (san_francisco_pop_density > rio_de_janeiro_pop_density) #replace `None` with your code
+print(comparision_result)
