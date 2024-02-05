13ae73451507f37841da29dd2b63bf799cf9c552
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-02-05 11:31:08 -0600

    Added the code

--- 06OOP's/03shirt.py
+++ 06OOP's/03shirt.py
@@ -1,11 +1,12 @@
 from shirt import Shirt
 
-shirt_one  = Shirt('red','M','long sleeved',45)
+
+shirt_one  = Shirt('red','M','long sleeved',45) # Using the object, 
 shirt_two  = Shirt('orange','S','short sleeved',30)
 print(shirt_one.price)
 print(shirt_one.color)
 shirt_two.change_price(45)
 print(shirt_two.price)
-shirt_one.color = 'yellow'
+shirt_one.color = 'yellow' # Prints the color 
 shirt_one.size = 'L'
-shirt_one.price = 38  # prints numbet
+shirt_one.price = 38  # prints number
